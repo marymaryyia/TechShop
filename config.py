a97bf8237252087a305b6473a9606514ddd84383
Mari <mariakashavanidze@gmail.com>
2026-07-17 22:22:49 +0400

    commit

--- config.py
+++ config.py
@@ -20,8 +20,9 @@ class Config:
     }
 
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
     
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
