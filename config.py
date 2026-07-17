8fe77b18a8db2786a99691a60c6697e27bccfdae
Mari <mariakashavanidze@gmail.com>
2026-07-17 15:56:43 +0400

    commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key-for-dev')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 'sqlite:///techshop.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
