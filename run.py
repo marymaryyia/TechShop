2484fcf495831412abd724797a29a3708a83ed00
Mari <mariakashavanidze@gmail.com>
2026-07-17 16:13:05 +0400

    m

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
-
+from extensions import db
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+    print("მონაცემთა ბაზა და ცხრილები წარმატებით შეიქმნა!")
 
 if __name__ == "__main__":
     app.run(debug=True)
